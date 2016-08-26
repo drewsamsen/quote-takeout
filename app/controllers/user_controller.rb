176e720cc3be56526947bbb42d57ccc7014d4f06
Drew Samsen <drewsamsen@gmail.com>
2016-08-25 21:31:10 -0700

    = debug users route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,17 @@
 class UserController < ApplicationController
 
-  before_filter :check_admin
+  # before_filter :check_admin
 
   def index
     @users = User.all
-    respond_with(users: @users)
+    # respond_with(users: @users)
+    respond_to do |format|
+      format.json {
+        render :json => {
+          :users => @users
+        }
+      }
+    end
   end
 
 end
